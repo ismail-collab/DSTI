e250bcdb09166012b50bac2891b22044405aeb87
Ismail <78820790+ismail-collab@users.noreply.github.com>
2024-04-12 22:48:21 +0200

    Delete Warm UP/Python/Beginner directory

--- Warm UP/Python/Beginner/Part 2 Web-Scraping/book-scrapper/tests/test_soup.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from book_scrapper.soup_parsing import get_soup
-
-def test_soup_conversion():
-    html_code = "<p>Test</p>"
-    soup = get_soup(html_code)
-
-    assert str(type(soup)) == "<class 'bs4.BeautifulSoup'>"
--- Warm UP/Python/Beginner/Part 2 Web-Scraping/book-scrapper/tests/test_url_get.py
+++
@@ -1,6 +0,0 @@
-from book_scrapper.url_get import url_get
-
-def test_url_get():
-    url = 'www.google.com'
-
-    assert 1 == 1
